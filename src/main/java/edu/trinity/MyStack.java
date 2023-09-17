c51756088508348160c3f1af455b44019d90fe80
mhsr21 <chris.d.d.yi@gmail.com>
2023-09-17 15:29:50 -0400

    Implement MyStackTest

--- src/main/java/edu/trinity/MyStack.java
+++ src/main/java/edu/trinity/MyStack.java
@@ -1,18 +1,37 @@
 package edu.trinity;
 
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+
 public class MyStack<T> {
+    private ArrayList<T> arr;
+    private int top;
+
+    public MyStack() {
+        arr = new ArrayList<T>();
+        this.top = -1;
+    }
     public void push(T hello) {
+        if (top + 1 == Integer.MAX_VALUE) System.out.println("Stack Overflow");
+        else {
+            top++;
+            if(arr.size() > top) arr.set(top, hello);
+            else arr.add(hello);
+        }
     }
 
     public T peek() {
-        return null;
+        if(top == -1) return null;
+        else return arr.get(top);
     }
 
     public T pop() {
-        return null;
+        if (top == -1) throw new EmptyStackException();
+        top--;
+        return arr.get(top + 1);
     }
 
     public boolean isEmpty() {
-        return false;
+        return (top < 0);
     }
 }
