528a79b8de67d00f93f4eda08b28b8e597337e4f
mhsr21 <chris.d.d.yi@gmail.com>
2023-10-02 19:24:42 -0400

    fix exception msg

--- src/main/java/edu/trinity/Fibonacci.java
+++ src/main/java/edu/trinity/Fibonacci.java
@@ -3,7 +3,7 @@ package edu.trinity;
 public class Fibonacci {
 
     public static int calculate(int i) {
-        if (i < 0 ) throw(new IllegalArgumentException("Must give value over 0"));
+        if (i < 0 ) throw(new IllegalArgumentException("Must give non-negative value"));
         else if (i < 2) return i;
         else return calculate(i - 1) + calculate(i - 2);
     }
