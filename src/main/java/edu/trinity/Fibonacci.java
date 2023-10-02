e8ff90524055959f3df37c87f51580420e5789bd
mhsr21 <chris.d.d.yi@gmail.com>
2023-10-02 19:21:21 -0400

    add exception for fibonacci

--- src/main/java/edu/trinity/Fibonacci.java
+++ src/main/java/edu/trinity/Fibonacci.java
@@ -3,7 +3,8 @@ package edu.trinity;
 public class Fibonacci {
 
     public static int calculate(int i) {
-        if (i < 2) return i;
+        if (i < 0 ) throw(new IllegalArgumentException("Must give value over 0"));
+        else if (i < 2) return i;
         else return calculate(i - 1) + calculate(i - 2);
     }
 }
